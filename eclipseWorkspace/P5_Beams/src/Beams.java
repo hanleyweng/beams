53b31f7c0264e9d5f120d56a95c638d07d4f7d15
Hanley Weng <hanleyweng@gmail.com>
2014-08-08 21:25:59 +1000

    First Commit - Basic P5 Sketch up

---
+++ eclipseWorkspace/P5_Beams/src/Beams.java
@@ -0,0 +1,18 @@
+import processing.core.*; // This is from Processing2.2.1
+
+@SuppressWarnings("serial")
+public class Beams extends PApplet {
+
+	int swidth = 800;
+	int sheight = 600;
+
+	public void setup() {
+		size(swidth, sheight);
+		colorMode(HSB, 100);
+	}
+
+	public void draw() {
+		ellipse(swidth / 2, sheight / 2, 50, 50);
+	}
+
+}
